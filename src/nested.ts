ae8c8d35a1081e3ac11dc4d6e5bcdefa84c2920e
Charmaine <charpasi@udel.edu>
2024-02-24 21:58:53 -0500

    findQuestion

--- src/nested.ts
+++ src/nested.ts
@@ -35,7 +35,10 @@ export function findQuestion(
     questions: Question[],
     id: number
 ): Question | null {
-    return null;
+    const found = questions.find(
+        (question: Question): boolean => question.id === id
+    );
+    return found === undefined ? null : found;
 }
 
 /**
