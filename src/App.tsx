d7394a9f5794d9eb534bbd982f69de0d8cb3e0ad
Charmaine <charpasi@udel.edu>
2024-03-01 11:34:13 -0500

    removed unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import "./App.css";
+import { ChangeType } from "./components/ChangeType";
+import { Counter } from "./components/Counter";
+import { CycleHoliday } from "./components/CycleHoliday";
+import { RevealAnswer } from "./components/RevealAnswer";
+import { StartAttempt } from "./components/StartAttempt";
+import { TwoDice } from "./components/TwoDice";
 
 function App(): JSX.Element {
     return (
