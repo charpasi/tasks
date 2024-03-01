177aacdeb57132d4a12f6c002a46f889ca946c58
Charmaine <charpasi@udel.edu>
2024-03-01 13:15:41 -0500

    ChangeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,22 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [question, setType] = useState<QuestionType>("short_answer_question");
+    const changeQuestion = (): void => {
+        setType(
+            question === "short_answer_question"
+                ? "multiple_choice_question"
+                : "short_answer_question"
+        );
+    };
+    return (
+        <div>
+            <Button onClick={changeQuestion}>Change Type</Button>{" "}
+            {question === "short_answer_question" ? (
+                <div>Short Answer</div>
+            ) : (
+                <div>Multiple Choice</div>
+            )}
+        </div>
+    );
 }
