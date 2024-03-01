41f43c6cdbf75bde2712e1d483c6e3abaf55aaf0
Charmaine <charpasi@udel.edu>
2024-03-01 11:29:28 -0500

    removed old html

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import "./App.css";
-import { Button, Col, Container, Row } from "react-bootstrap";
-import mickey from "./mickey.jpg";
-console.log(mickey);
 function App(): JSX.Element {
     return (
         <div className="App">
@@ -13,45 +10,6 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-            <h1>Task 3</h1>
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-            <p>Charmaine</p>
-            <p>Hello World!</p>
-            <Container>
-                <Row>
-                    <Col>
-                        <div
-                            style={{
-                                height: "200px",
-                                width: "100px",
-                                backgroundColor: "red"
-                            }}
-                        ></div>
-                        <ul>
-                            <li>Item 1</li>
-                            <li>Item 2</li>
-                            <li>Item 3</li>
-                        </ul>
-                    </Col>
-                    <Col>
-                        <div
-                            style={{
-                                height: "200px",
-                                width: "100px",
-                                backgroundColor: "red"
-                            }}
-                        ></div>
-                        <img
-                            src={mickey}
-                            alt="A picture of my dog Mickey"
-                            width="531"
-                            height="537"
-                        />
-                    </Col>
-                </Row>
-            </Container>
         </div>
     );
 }
