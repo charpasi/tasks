ff425a9e13d4c97126eb29837600ad8c0ada5680
Charmaine <charpasi@udel.edu>
2024-03-05 17:12:15 -0500

    twoDice

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,45 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState<number>(4);
+    const removeAttempt = () => {
+        setAttempts(attempts - 1);
+    };
+    const addAttempt = () => {
+        setAttempts(attempts + 1);
+    };
+    const [inProgress, setInProgress] = useState<boolean>(false);
+    const changeInProgress = () => {
+        setInProgress(!inProgress);
+    };
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    removeAttempt();
+                    changeInProgress();
+                }}
+                disabled={inProgress === true || attempts === 0}
+            >
+                Start Quiz
+            </Button>
+            {attempts}
+            <Button
+                onClick={() => changeInProgress()}
+                disabled={inProgress === false}
+            >
+                Stop Quiz
+            </Button>
+            {attempts}
+            <Button
+                disabled={inProgress === true}
+                onClick={() => {
+                    addAttempt();
+                }}
+            >
+                Mulligan
+            </Button>
+            {attempts}
+        </div>
+    );
 }
