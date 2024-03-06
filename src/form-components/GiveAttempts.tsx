45307af0eca4cb7270f81340379f9e4c4bd9fb02
Charmaine <charpasi@udel.edu>
2024-03-06 17:39:33 -0500

    giveAttempts

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,33 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [remaining, setRemaining] = useState<number>(3);
+    const [requested, setRequested] = useState<string>("0");
+    const requestedAttempts = parseInt(requested) || 0;
+    function use(): void {
+        setRemaining(remaining - 1);
+    }
+    function gain(): void {
+        setRemaining(remaining + requestedAttempts);
+    }
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <Form.Group controlId="formMovieReleased">
+                <Form.Label>Requested:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={requested}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setRequested(event.target.value)
+                    }
+                />
+            </Form.Group>
+            <Button disabled={remaining === 0} onClick={use}>
+                use
+            </Button>
+            <Button onClick={gain}>gain</Button>
+            <div>{remaining}</div>
         </div>
     );
 }
