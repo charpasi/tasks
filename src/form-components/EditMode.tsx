3c64ebe9fe266f1852a1302401dbd04ef59e91df
Charmaine <charpasi@udel.edu>
2024-03-06 21:20:21 -0500

    editMode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,53 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [editing, setEditing] = useState<boolean>(false);
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+    const [username, setUsername] = useState<string>("Your Name");
+    function updateIsStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsStudent(event.target.checked);
+    }
+    function updateEditing(event: React.ChangeEvent<HTMLInputElement>) {
+        setEditing(event.target.checked);
+    }
+    function updateName(event: React.ChangeEvent<HTMLInputElement>) {
+        setUsername(event.target.value);
+    }
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <Form.Switch
+                type="switch"
+                id="is-editing-check"
+                label="Editing?"
+                checked={editing}
+                onChange={updateEditing}
+            />
+            {editing && (
+                <Form.Check
+                    type="checkbox"
+                    id="is-student"
+                    label="Student?"
+                    checked={isStudent}
+                    onChange={updateIsStudent}
+                />
+            )}
+            <div>
+                {editing && (
+                    <Form.Group controlId="username">
+                        <Form.Label>Name:</Form.Label>
+                        <Form.Control
+                            value={username}
+                            onChange={updateName}
+                            disabled={!isStudent}
+                        />
+                    </Form.Group>
+                )}
+                {((editing && isStudent) || isStudent) &&
+                    username + " is a student"}
+                {((!editing && !isStudent) || !isStudent) &&
+                    username + " is not a student"}
+            </div>
         </div>
     );
 }
